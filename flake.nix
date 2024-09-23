484d2b6d0f0888f92d3c4d5d0f2d1ae0e6a85b29
Cainã Costa <me@cfcosta.com>
2024-09-23 19:22:12 -0300

    chore: clean up nix file

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Hydra control plane";
+  description = "A Chocolate Doom based bot client";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -15,24 +15,11 @@
     };
   };
 
-  nixConfig = {
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    ];
-    substituters = [
-      "https://cache.nixos.org/"
-      "https://cache.iog.io/"
-    ];
-  };
-
   outputs =
     {
       nixpkgs,
       rust-overlay,
       flake-utils,
-      cardano-node,
-      hydra,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -45,9 +32,6 @@
         };
 
         inherit (pkgs) makeRustPlatform mkShell rust-bin;
-        inherit (pkgs.lib) optionals;
-        inherit (pkgs.stdenv) isDarwin;
-
         rust = rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
         platform = makeRustPlatform {
@@ -57,44 +41,26 @@
       in
       {
         packages.default = platform.buildRustPackage {
-          name = "hydra-control-plane";
+          name = "hydra-bot";
           src = ./.;
-          buildInputs =
-            with pkgs;
-            (
-              [
-                pkg-config
-                openssl
-              ]
-              ++ optionals isDarwin [
-                darwin.apple_sdk.frameworks.SystemConfiguration
-              ]
-            );
-          cargoLock = {
-            lockFile = ./Cargo.lock;
 
-            outputHashes = {
-              "pallas-0.29.0" = "sha256-P//R/17kMaqN4JGHFFTMy2gbo7k+xWUaqkF0LFVUxWQ=";
-            };
-          };
+          buildInputs = with pkgs; [
+            pkg-config
+            openssl
+          ];
+
+          cargoLock.lockFile = ./Cargo.lock;
         };
 
         devShells.default = mkShell {
-          buildInputs =
-            [
-              # Runtime dependencies
-              cardano-node.packages.${system}.cardano-cli
-              hydra.packages.${system}.hydra-node
+          buildInputs = with pkgs; [
+            rust
 
-              # Rust Environment
-              rust
-              pkgs.pkg-config
-              pkgs.openssl
-              pkgs.python312Packages.virtualenvwrapper
-            ]
-            ++ optionals isDarwin [
-              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-            ];
+            chocolate-doom
+            openssl
+            pkg-config
+            python312Packages.virtualenvwrapper
+          ];
 
           shellHook =
             let
@@ -115,6 +81,7 @@
 
               source ./.venv/bin/activate
               export PYTHONPATH=`pwd`/.venv/${pkgs.python312.sitePackages}/:$PYTHONPATH
+              [ -e .venv/bin/aider ] || pip install aider
             '';
         };
       }
