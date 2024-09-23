500cbe37131e2a0dfaf4675ff8377a30aea370eb
Cainã Costa <me@cfcosta.com>
2024-09-23 18:40:16 -0300

    chore: Rename d_loop.rs to game.rs and create a Game struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-mod d_loop;
+mod game;
 mod net_client;
 mod net_packet;
 mod net_structs;
@@ -8,6 +8,7 @@ mod net_structs;
 use tracing::info;
 
 use self::net_client::NetClient;
+use self::game::Game;
 
 fn main() {
     tracing_subscriber::fmt::init();
@@ -16,13 +17,15 @@ fn main() {
     let mut client = NetClient::new("Player1".to_string(), false);
     client.init();
 
-    info!("Client initialized, starting main loop");
+    info!("Initializing game");
+    let mut game = Game::new();
 
-    d_loop::init();
-    d_loop::d_start_game_loop();
+    info!("Client and game initialized, starting main loop");
+
+    game.d_start_game_loop();
 
     loop {
-        d_loop::try_run_tics(&mut client);
+        game.try_run_tics(&mut client);
 
         // Add some delay to prevent busy-waiting
         std::thread::sleep(std::time::Duration::from_millis(10));
